11847d940af38cc0af655a42c34c6a5820369b14
mohammadbassim <mohaabdelhai@gmail.com>
2025-05-14 21:12:53 +0400

    as

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://localhost:7094/api',
+    baseURL: 'https://gnosis-001-site2.jtempurl.com/api',
 });
 
 api.interceptors.request.use((config) => {
